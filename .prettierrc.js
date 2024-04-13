35286fb983e8686aa71fea9e229bf96fec3d9810
atlasfoo <iscomejia15@outlook.com>
2024-04-12 22:17:36 -0600

    initial commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,10 @@ module.exports = {
   printWidth: 100,
   tabWidth: 2,
   singleQuote: true,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
   bracketSameLine: true,
   trailingComma: 'es5',
-};
+  semi: false,
+  endOfLine: 'auto'
+}
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,11 @@
-import { forwardRef } from 'react';
+import { forwardRef } from 'react'
 
-import { Button as TButton, ButtonText } from '../tamagui.config';
+import { Button as TButton, ButtonText } from '../tamagui.config'
 
 export const Button = forwardRef<TouchableOpacity, ButtonProps>(({ onPress, title }, ref) => {
   return (
     <TButton onPress={onPress}>
       <ButtonText>{title}</ButtonText>
     </TButton>
-  );
-});
+  )
+})
